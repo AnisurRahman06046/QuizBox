e9c44d1c62fedb29fbe36283428f7769035c314c
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-10-11 08:35:54 +0600

    page title,header with img and site info in home route added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,17 @@ import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <h1>homepage</h1>
+      <div className="home-banner">
+        <h2>QuizBox</h2>
+        <p>
+          Hi Heroes,this site is for the programmer who have strong basics about
+          the programming languages.Here,you can justify your basics by
+          participating several Quizes .
+          <span className="para-text">
+            So,are you ready to take challenges ?
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
