9f5c374c17ff37c2b8dd934836e747eac1f9477c
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-10-12 01:35:49 +0600

    onclick btn added to get the ans value

--- src/components/QuizandOptions/QuizandOptions.js
+++ src/components/QuizandOptions/QuizandOptions.js
@@ -4,27 +4,30 @@ import "./Quizandoptions.css";
 const QuizandOptions = ({ q }) => {
   //   console.log(question);
   const { question, options } = q;
-  console.log(question);
-  console.log(options);
+  //   console.log(question);
+  //   console.log(options);
+  const btnHandle = (e) => {
+    console.log(e);
+  };
 
   return (
     <div>
       <div className="quiz-container">
         <h3>{question.replace(/(<([^>]+)>)/gi, "")}</h3>
         <div className="quiz-answer-option">
-          <div className="single-option">
+          <div onClick={() => btnHandle(options[0])} className="single-option">
             <input type="radio" name="option1" value={options[0]} id="" />
             {options[0]}
           </div>
-          <div className="single-option">
+          <div onClick={() => btnHandle(options[1])} className="single-option">
             <input type="radio" name="option1" value={options[1]} id="" />
             {options[1]}
           </div>
-          <div className="single-option">
+          <div onClick={() => btnHandle(options[2])} className="single-option">
             <input type="radio" name="option1" value={options[2]} id="" />
             {options[2]}
           </div>
-          <div className="single-option">
+          <div onClick={() => btnHandle(options[3])} className="single-option">
             <input type="radio" name="option1" value={options[3]} id="" />
             {options[3]}
           </div>
