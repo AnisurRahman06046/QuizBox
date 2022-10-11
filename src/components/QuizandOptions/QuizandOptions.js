82a7104db0d3f75a22be0abbc3c09b75ce3e8d9b
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-10-12 01:21:28 +0600

    quiz card updated

--- src/components/QuizandOptions/QuizandOptions.js
+++ src/components/QuizandOptions/QuizandOptions.js
@@ -10,16 +10,24 @@ const QuizandOptions = ({ q }) => {
   return (
     <div>
       <div className="quiz-container">
-        <p>{question.replace(/(<([^>]+)>)/gi, "")}</p>
+        <h3>{question.replace(/(<([^>]+)>)/gi, "")}</h3>
         <div className="quiz-answer-option">
-          <input type="radio" name="option1" value={options[0]} id="" />
-          {options[0]} <br />
-          <input type="radio" name="option1" value={options[1]} id="" />
-          {options[1]} <br />
-          <input type="radio" name="option1" value={options[2]} id="" />
-          {options[2]} <br />
-          <input type="radio" name="option1" value={options[3]} id="" />
-          {options[3]} <br />
+          <div className="single-option">
+            <input type="radio" name="option1" value={options[0]} id="" />
+            {options[0]}
+          </div>
+          <div className="single-option">
+            <input type="radio" name="option1" value={options[1]} id="" />
+            {options[1]}
+          </div>
+          <div className="single-option">
+            <input type="radio" name="option1" value={options[2]} id="" />
+            {options[2]}
+          </div>
+          <div className="single-option">
+            <input type="radio" name="option1" value={options[3]} id="" />
+            {options[3]}
+          </div>
         </div>
       </div>
     </div>
