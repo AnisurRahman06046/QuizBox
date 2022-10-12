092e20c18cbc9e1fbc8aa265cb4ae8854e97c950
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-10-12 07:18:35 +0600

    blog route added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,7 +4,56 @@ import "./Blog.css";
 const Blog = () => {
   return (
     <div>
-      <h1>blog page</h1>
+      <h1>Blog</h1>
+      <section>
+        <div className="blog">
+          <h3>What is the purpose of React Router ?</h3>
+          <p>
+            ReactJS Router is mainly used for developing Single Page Web
+            Applications. React Router is used to define multiple routes in the
+            application. When a user types a specific URL into the browser, and
+            if this URL path matches any 'route' inside the router file, the
+            user will be redirected to that particular route. React Router plays
+            an important role to display multiple views in a single page
+            application. Without React Router, it is not possible to display
+            multiple views in React applications. Most of the social media
+            websites like Facebook, Instagram uses React Router for rendering
+            multiple views.
+          </p>
+        </div>
+        <div className="blog">
+          <h3>How does context api work?</h3>
+          <p>
+            The React Context API is a way for a React app to effectively
+            produce global variables that can be passed around. This is the
+            alternative to "prop drilling" or moving props from grandparent to
+            child to parent, and so on. Context is also touted as an easier,
+            lighter approach to state management using Redux. It returns a
+            consumer and a provider. Provider is a component that as it's names
+            suggests provides the state to its children. It will hold the
+            "store" and be the parent of all the components that might need that
+            store. Consumer as it so happens is a component that consumes and
+            uses the state.
+          </p>
+        </div>
+        <div className="blog">
+          <h3>What is useRef ?</h3>
+          <p>
+            Ref means just reference, so it can be a reference to anything (DOM
+            node, Javascript value, etc). The useRef hook returns a mutable
+            object which doesn’t re-render the component when it’s changed.
+            Think it like useState, but unlike useState, doesn’t trigger
+            re-render of the component. The object that useRef returns have a
+            current property that can hold any modifiable value. The useRef
+            returns a mutable ref object. This object has a property called
+            .current. The value is persisted in the refContainer.current
+            property. These values are accessed from the current property of the
+            returned object. The .current property could be initialised to the
+            passed argument initialValue e.g. useRef(initialValue). The object
+            can persist a value for a full lifetime of the component.{" "}
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -18,32 +18,34 @@ const Statistics = () => {
   console.log(data);
 
   return (
-    <div className="statistics">
+    <div>
       <h1>Quiz Statistics</h1>
-      <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis dataKey="total" />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="name"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-      </LineChart>
+      <div>
+        <LineChart
+          width={800}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis dataKey="total" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="name"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+        </LineChart>
+      </div>
     </div>
   );
 };
